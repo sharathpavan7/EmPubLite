26743cd0cc82adad04f75adb942786eda50cab70
Sharath-pc\sharath <sharathpavan7@gmail.com>
2017-02-18 23:38:26 +0530

    initial commit

---
+++ app/src/main/java/com/sharath/empublite/EmPubLiteActivity.java
@@ -0,0 +1,13 @@
+package com.sharath.empublite;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EmPubLiteActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
